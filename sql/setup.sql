affbf59513b663996a03a35a60cd1be551df180a
Juan Ortiz <jortlpez@gmail.com>
2025-08-20 23:13:38 +0200

    Store contacts

--- sql/setup.sql
+++ sql/setup.sql
@@ -12,4 +12,12 @@ CREATE TABLE contacts (
 
 INSERT INTO contacts (name, phone_number) VALUES ("Pepe", "123456789"); 
 
+INSERT INTO contacts (name, phone_number) VALUES ("Juan", "4456789"); 
+
+INSERT INTO contacts (name, phone_number) VALUES ("Carmen", "565456789"); 
+
+INSERT INTO contacts (name, phone_number) VALUES ("Pili", "23446789"); 
+
+INSERT INTO contacts (name, phone_number) VALUES ("Nata", "234219"); 
+
 
